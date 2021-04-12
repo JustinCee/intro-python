1c321beaf19e937f7ea6a4084b2af6f12c80581f
Justincee <calvertjustin1996@gmail.com>
2021-04-12 16:25:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Task One
+print("Hello World!")
+
+# Task two
+_PI = 3.14
+radius = int(input('Please type in the radius: '))
+area = _PI * radius**2
+print("the area of a circle is: " + str(area))
+
+# Task three
+radius = int(input('Please type in the radius: '))
+circumference = 2 * _PI * radius
+print("the circumference of a circle is: " + str(circumference))
